7b62c2d468deca45dcad283e07deb3c47dba9fd4
Joschuan Santana <santanaelectronica@gmail.com>
2023-09-06 15:25:19 -0300

    Include presign public url

--- src/lambda/dataset-microservice/controllers/dataset_controller.py
+++ src/lambda/dataset-microservice/controllers/dataset_controller.py
@@ -1,9 +1,14 @@
 import urllib.parse
 from aws_lambda_powertools import Tracer
 from aws_lambda_powertools.event_handler.router import APIGatewayRouter
+from aws_lambda_powertools.event_handler.exceptions import (
+    BadRequestError,
+    UnauthorizedError
+)
 
-# from infra_commons.services.storage_service import StorageService
+from infra_commons.services.storage_service import StorageService
 from services.dataset_service import DatasetService
+from models.dataset import AccessLevelEnum, RepositoryTypeEnum
 
 tracer = Tracer()
 router = APIGatewayRouter()
@@ -11,9 +16,21 @@ router = APIGatewayRouter()
 
 @router.get('/<datasetSlug>/signedUrl')
 @tracer.capture_method
-def retrieve_dataset(datasetSlug: str):
+def get_public_dataset_signed_url(datasetSlug: str):
     dataset_service = DatasetService(router.context.get('dynamodb_resource'))
+    storage_service = StorageService(router.context.get('s3_resource'))
 
-    return dataset_service.retrieve_by_slug(
-        urllib.parse.unquote_plus(datasetSlug)
+    decoded_dataset_slug = urllib.parse.unquote_plus(datasetSlug)
+    dataset = dataset_service.retrieve_by_slug(
+        decoded_dataset_slug.lower()
     )
+
+    if dataset.AccessLevel != AccessLevelEnum.PUBLIC:
+        raise UnauthorizedError('This dataset is not publicly accessible')
+
+    if dataset.RepositoryType != RepositoryTypeEnum.AMAZON_S3:
+        raise BadRequestError('Dataset RepositoryType must be "amazon-s3" to return signed url')
+
+    return {
+        'url': storage_service.generate_signed_download_url(dataset.DatasetConfig)
+    }
--- src/layers/endpoints-dependencies/infra_commons/services/storage_service.py
+++ src/layers/endpoints-dependencies/infra_commons/services/storage_service.py
@@ -1,13 +1,13 @@
 
 from infra_commons.aws_resources import LambdaS3
-from infra_commons.models.s3_object import InputS3Content
+from infra_commons.models.s3_object import InputS3Content, S3ObjectReference
 
 
 class StorageService:
     def __init__(self, resource: LambdaS3):
         self.resource = resource
 
-    def sign_upload_url(self, bucket, key):
+    def sign_upload_url(self, bucket: str, key: str):
         return self.resource.client.generate_presigned_url(
             ClientMethod='put_object',
             Params={
@@ -28,3 +28,13 @@ class StorageService:
             presigned_content = self.sign_upload_url(input_reference.Content.Bucket, input_reference.Content.Key)
 
         return presigned_content
+
+    def generate_signed_download_url(self, object_reference: S3ObjectReference):
+        return self.resource.client.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket': object_reference.Bucket,
+                'Key': object_reference.Key
+            },
+            ExpiresIn=3600
+        )
